036f84b6785564c5b5efe037b2a1ed627d474c6b
unknown <sshelkov1985@gmail.com>
2023-01-12 10:43:13 +0300

    feat: my index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result
+    if (n <= 0){
+      return parseInt(n.toString().split("").reverse().join(""));
+    }
+       else if (!parseInt(n)) {
+              return ('Need to write a number!')}
+      
+      else {
+        return (n.toString().split("").reverse().join(""));}
+      return result
 }
